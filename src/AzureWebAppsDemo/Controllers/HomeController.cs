5996fabe3c4f6cce7ae8917668b689c8806ed180
Steve Michelotti <steve.michelotti@gmail.com>
2016-09-26 17:11:54 -0400

    added logging

--- src/AzureWebAppsDemo/Controllers/HomeController.cs
+++ src/AzureWebAppsDemo/Controllers/HomeController.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AzureWebAppsDemo.Controllers
 {
     public class HomeController : Controller
     {
         private IConfiguration config;
+        private ILogger<HomeController> logger;
 
-        public HomeController(IConfiguration config)
+        public HomeController(IConfiguration config, ILogger<HomeController> logger)
         {
             this.config = config;
+            this.logger = logger;
         }
 
         public IActionResult Index()
         {
+            this.logger.LogInformation("**This is my cool info message!");
+            this.logger.LogWarning("**This is my cool *warning* message!");
+
             this.ViewBag.SubTitle = this.config["General:subtitle"];
             return View();
         }
